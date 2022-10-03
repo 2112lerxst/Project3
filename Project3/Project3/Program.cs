5359bf557d87aae7f6743c9004a88e302ebc9a52
Jerry <103043880+2112lerxst@users.noreply.github.com>
2022-10-03 13:51:01 -0400

    Add files via upload

---
+++ Project3/Project3/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Project3
+{
+    internal class Program
+    {
+
+
+        //It doesn't work and I don't know how to fix it.
+        //Take user input to look for file
+        //Write line
+        //Create new jagged array
+        //Look for file with streamreader(?)
+        static void Main(string[] args)
+        {
+            string uInput = methodman.Read("Enter file: ");
+            Console.WriteLine(uInput);
+            int[][] jaggedArray = new int[20][];
+            StreamReader sr = new StreamReader(Path.GetFullPath("inputJagged.csv"));
+
+
+        }
+
+        //Sort method. Method is taken from: https://code-maze.com/csharp-merge-sort/ its the only one I understood how to do
+        //Left side greater than right.
+        //Middle is value looking for
+        //Left is left boundary
+        //Right is right boundary
+
+        public int[] SortArray(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                SortArray(array, left, middle);  //Calls sort to sort left side
+                SortArray(array, middle + 1, right); //Calls sort to right side
+                MergeArray(array, left, middle, right);
+            }
+            return array;
+        }
+
+
+        //Method merges array.
+        //Takes 4 values
+        //Array is value needed to be sorted
+        //left boundary
+        //right boundary
+        //middle pivot point
+        public void MergeArray(int[] array, int left, int middle, int right) 
+        {
+            var leftArrayLength = middle - left + 1;
+
+            var rightArrayLength = right - middle;
+
+            var leftTempArray = new int[leftArrayLength]; //left
+
+            var rightTempArray = new int[rightArrayLength]; // right array
+
+            int i, j;
+            for (i = 0; i < leftArrayLength; ++i) //Fill left array
+                leftTempArray[i] = array[left + i];
+           
+            for (j = 0; j < rightArrayLength; ++j)        //Fill right array
+               rightTempArray[j] = array[middle + 1 + j];
+            
+            i = 0; //i becomes left erray
+           
+            j = 0; //j becomes right
+           
+            int k = left; //
+            while (i < leftArrayLength && j < rightArrayLength) //Left now gets compared to right
+            {
+                if (leftTempArray[i] <= rightTempArray[j]) //Left smaller
+                {
+                    array[k++] = leftTempArray[i++]; //Left added to sorted array
+                }
+                else //right
+                {
+                    array[k++] = rightTempArray[j++]; //right added to sorted array
+                }
+            }
+           
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+           
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+
+
+    } }
---
+++ Project3/Project3/methodman.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project3
+{
+    internal class methodman
+    {
+        //Sorting method to ask for user input.
+        public static string Read( string input)
+        {
+            string val = " "; // Start with empty value for input
+            string vvStr = Console.ReadLine();
+        return val; 
+        } //Return value
+        public static int[][] fileJag(int[][] jaggedarray, string nuInput)// Create a new input for the jagged array for the file.csv
+        {
+
+            //https://regexlib.com/REDetails.aspx?regexp_id=345&AspxAutoDetectCookieSupport=1 used this to filter file paths
+            //https://stackoverflow.com/questions/6416065/c-sharp-regex-for-file-paths-e-g-c-test-test-exe also used this to determine file paths
+            string file = @"(^([a-z]|[A-Z]):(?=\\(?![\0-\37<>:""/\\|?*])|\/(?![\0-\37<>:""/\\|?*])|$)|^\\(?=[\\\/][^\0-\37<>:""    /\\|?*]+)|^(?=(\\|\/)$)|^\.(?=(\\|\/)$)|^\.\.(?=(\\|\/)$)|^(?=(\\|\/)[^\0-\37<>:""/\\|?*]+)|^\.(?=(\\|\/)[^\0-\37<>:""  /\\|?*]+)|^\.\.(?=(\\|\/)[^\0-\37<>:""/\\|?*]+))((\\|\/)[^\0-\37<>:""/\\|?*]+|(\\|\/)$)*()$";
+         
+           
+        }
+    }
+}
